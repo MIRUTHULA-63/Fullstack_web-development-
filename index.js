fbcbf013c8d4ec330f74d1d451c9e6682fac8553
MIRUTHULA-63 <miruthuladhanasekar@gmail.com>
2024-10-03 15:10:28 +0530

    customer model added

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bodyparser from "body-parser";
-import crudroutes from "./routes/crudroutes/index.js"
+import crudroutes from "./routes/crudroutes/index.js";
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
 app.use(bodyparser.json());
+app.use("/customer", crudroutes);
+
+app.use("/restaurant", crudroutes);
+
 mongoose
   .connect(
     "mongodb+srv://miruthuladhanasekar:1234@cluster0.syzzf.mongodb.net/restaur"
